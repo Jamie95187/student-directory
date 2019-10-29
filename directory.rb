9535451de3487293a9de7ef43f5fe5341e784dfb
Jamie <jamiewong63@googlemail.com>
2019-10-29 21:05:24 +0000

    Changed each loop to while loop, prints students in reverse order, and changed print_footer method to accommodate code

--- directory.rb
+++ directory.rb
@@ -5,12 +5,18 @@ end
 def print_with_criteria(students)
   # puts "Enter letter you want filter by"
   # letter = gets.chomp.capitalize
-  students.each_with_index do |student, index|
-    puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
+  # students.each_with_index do |student, index|
+  #   puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
+  # end
+  count = 1
+  while !students.empty?
+    student = students.pop
+    puts "#{count} #{student[:name]} (#{student[:cohort]} cohort)"
+    count += 1
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(number)
+  puts "Overall, we have #{number} great students"
 end
 def input_students
   puts "Please enter the names of the students"
@@ -32,6 +38,7 @@ def input_students
 end
 #nothing happens until we call the methods
 students = input_students
+student_count = students.count
 print_header
 print_with_criteria(students)
-print_footer(students)
+print_footer(student_count)
