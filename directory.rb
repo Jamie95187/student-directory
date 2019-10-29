cf8b6700eab3498695b25029a5d02fb42ac83f8a
Jamie <jamiewong63@googlemail.com>
2019-10-29 21:45:54 +0000

    Added code to ask for more information about the students

--- directory.rb
+++ directory.rb
@@ -19,19 +19,28 @@ def print_footer(number)
   puts "Overall, we have #{number} great students"
 end
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names and information of the students"
+  puts "To finish enter an empty name"
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  puts "Please enter the name of the student"
   # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+  loop do
     name = gets.chomp
+    break if name.empty?
+    #Ask user to input the additional information of the student
+    puts "Please enter the hobby of the student"
+    hobby = gets.chomp
+    puts "Please enter the height of the student"
+    height = gets.chomp
+    puts "Please enter the country of birth of the student"
+    country_of_birth = gets.chomp
+    # add the student hash to the array
+    students << {name: name, cohort: :november, hobby: hobby, height: height, country_of_birth: country_of_birth}
+    puts "Now we have #{students.count} students"
+    # ask for another name from the user
+    puts "Please enter the name of the student"
   end
   # return the array of students
   students
